3efc78b830e19cad50a80c3e7f96fc3cda2a1f70
Zoltán Matók <zolo@zolos-macbook-pro.local>
2016-10-08 10:41:27 +0200

    Initial commit

---
+++ Muttley/Muttley.h
@@ -0,0 +1,19 @@
+//
+//  Muttley.h
+//  Muttley
+//
+//  Created by Zolo on 10/8/16.
+//  Copyright © 2016 Zolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Muttley.
+FOUNDATION_EXPORT double MuttleyVersionNumber;
+
+//! Project version string for Muttley.
+FOUNDATION_EXPORT const unsigned char MuttleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Muttley/PublicHeader.h>
+
+
---
+++ Muttley/Muttley.swift
@@ -0,0 +1,9 @@
+//
+//  Muttley.swift
+//  Muttley
+//
+//  Created by Zolo on 10/8/16.
+//  Copyright © 2016 Zolo. All rights reserved.
+//
+
+import Foundation
